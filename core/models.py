c1d0ede375e25a6cdc7964e7a919939adf824ba9
neelarya19 <neelarya19@gmail.com>
2021-12-28 20:27:53 +0530

    first

--- core/models.py
+++ core/models.py
@@ -149,7 +149,8 @@ class Certificate(models.Model):
     date = models.DateTimeField(blank=True, null=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     title = models.CharField(max_length=200, blank=True, null=True)
-    description = models.CharField(max_length=500, blank=True, null=True)
+    # description = models.CharField(max_length=500, blank=True, null=True)
+    description=RichTextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
