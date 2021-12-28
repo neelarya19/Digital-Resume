50908dc632ca961228cd13edce1a958675afe655
neelarya19 <neelarya19@gmail.com>
2021-12-29 03:29:56 +0530

    update

--- DigitalResume/settings.py
+++ DigitalResume/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR / 'staticfiles'
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR / 'medialfiles'
+MEDIA_ROOT=BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
