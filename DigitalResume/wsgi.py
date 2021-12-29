afd564b24327009b2fe2d6122a585dfc1d8b6a62
neelarya19 <neelarya19@gmail.com>
2021-12-30 02:57:38 +0530

    update

--- DigitalResume/wsgi.py
+++ DigitalResume/wsgi.py
@@ -16,6 +16,3 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DigitalResume.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
