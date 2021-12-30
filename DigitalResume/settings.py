d61b0cd557b1f11b0c8d933e44a6d4756cc8adc1
neelarya19 <neelarya19@gmail.com>
2021-12-30 21:04:26 +0530

    update

--- DigitalResume/settings.py
+++ DigitalResume/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
