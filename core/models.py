68fbd76a7c39041b1a4af1b0bbf0500a5c7c968a
neelarya19 <neelarya19@gmail.com>
2021-12-31 19:03:34 +0530

    update

--- core/models.py
+++ core/models.py
@@ -91,7 +91,7 @@ class Portfolio(models.Model):
     class Meta:
         verbose_name_plural = 'Portfolio Profiles'
         verbose_name = 'Portfolio'
-        ordering = ["name"]
+        ordering = ['id']
     date = models.DateTimeField(blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
