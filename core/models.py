224ee402ca4ce77d6264e3955df05fee5f4f1086
neelarya19 <neelarya19@gmail.com>
2022-01-06 00:23:22 +0530

    update

--- core/models.py
+++ core/models.py
@@ -27,7 +27,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(blank=True, null=True, upload_to="avatar")
     title = models.CharField(max_length=200, blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
+    bio = RichTextField(blank=True, null=True)
     skills = models.ManyToManyField(Skill, blank=True)
     cv = models.FileField(blank=True, null=True, upload_to="cv")
 
